a0c3610fa493453ca4b8ddc71bed829267b614b2
Fiona Yvette Baraza <61788237+YvetteFB@users.noreply.github.com>
2022-06-16 21:14:39 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,30 +7,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, rd, wd;
-	char *buff = malloc(sizeof(char) * letters);
+	int fd;
+	int s, t;
+	char *buf;
 
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (0);
-	}
 
-	fd = open(filename, O_RDWR);
-	if (fd == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
+	s = read(fd, buf, letters);
+	if (s < 0)
 	{
+		free(buf);
 		return (0);
 	}
-	rd = read(fd, buff, letters);
-	if (rd == -1)
-	{
-		return (0);
-	}
-	wd = write(STDOUT_FILENO, buff, rd);
-	if (wd == -1)
-	{
-		return (0);
-	}
+	buf[s] = '\0';
+
 	close(fd);
-	free(buff);
-	return (wd);
+
+	t = write(STDOUT_FILENO, buf, s);
+	if (t < 0)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	return (t);
 }
