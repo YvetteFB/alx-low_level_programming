2c1eec08b6356e81ead20ea5849e72045c6b4144
Fiona Yvette Baraza <61788237+YvetteFB@users.noreply.github.com>
2022-06-16 04:54:23 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,33 @@
 #include "holberton.h"
 
 /**
- * create_file - create a file with read/write access for user
- * @filename: name of file to create
- * @text_content: string to write to file
- * Return: 1 on success, -1 on failure
+ *  create_file - create a file and write in it
+ * @filename:file to be created
+ * @text_content: content of the file created
+ * Return: 1 or -1;
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, rstatus, i;
+	int fd, len, w;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
 	if (fd == -1)
-		return (-1);
-
-	if (text_content)
 	{
-		for (i = 0; text_content[i] != '\0'; i++)
-			;
-		rstatus = write(fd, text_content, i);
-		if (rstatus == -1)
-			return (-1);
+		return (-1);
 	}
-
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+		;
+		w = write(fd, text_content, len);
+	}
+	if (w == -1)
+	{
+		return (-1);
+	}
 	close(fd);
 	return (1);
 }
