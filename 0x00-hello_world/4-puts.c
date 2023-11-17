7af5858bef610d9a8f431de8f725b94660677d1e
Fiona Yvette Baraza <61788237+YvetteFB@users.noreply.github.com>
2023-11-17 10:52:51 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+//the main function prints a statement using the function puts
 int main(void)
 {
-    puts("Programming is like buiding a multilingual puzzle.\n");
+    puts("Programming is like buiding a multilingual puzzle\n");
     return 0;
 }
