cede22860341faecfd4e58f083a941e0a6f93c4a
Fiona Yvette Baraza <61788237+YvetteFB@users.noreply.github.com>
2023-11-17 10:59:45 +0300

    Escape character for "

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*the main function prints a statement using the function puts*/
 int main(void)
 {
-    puts('"Programming is like buiding a multilingual puzzle\n');
+    puts("\"Programming is like buiding a multilingual puzzle\n");
     return 0;
 }
