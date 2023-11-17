30a9f184f073f5aedb17f5e80f8a9e67c00794af
Fiona Yvette Baraza <61788237+YvetteFB@users.noreply.github.com>
2023-11-17 10:50:42 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
-#!/bin/bash
 #include <stdio.h>
 
 int main(void)
 {
-    puts("Programming is like buiding a multilingual puzzle.\n")
+    puts("Programming is like buiding a multilingual puzzle.\n");
     return 0;
 }
