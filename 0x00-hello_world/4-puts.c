535803e3e164e3869a7b0afefa57b7a33a1c5685
Fiona Yvette Baraza <61788237+YvetteFB@users.noreply.github.com>
2023-11-17 11:04:44 +0300

    used betty style coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/*the main function prints a statement using the function puts*/
+/**
+*main function - prints using puts
+**/
 int main(void)
 {
-    puts("\"Programming is like buiding a multilingual puzzle\n");
-    return 0;
+puts("\"Programming is like buiding a multilingual puzzle\n");
+return (0);
 }
