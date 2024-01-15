8341b106b9a29bc06126a84575017ee8f5850c4c
YvetteFB <fionabaraza@gmail.com>
2024-01-15 05:35:42 -0800

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: returns 1
+*/
+int main(void)
+{
+char currentLetter = 'z';
+while (currentLetter >= 'a')
+{
+putchar(currentLetter);
+currentLetter++;
+}
+putchar('\n');
+return (0);
+}
