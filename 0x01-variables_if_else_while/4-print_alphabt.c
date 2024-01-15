bd94bd5ca6c7eed1fc466b5ddc0d8e40f6efa3cd
Fiona Yvette Baraza <61788237+YvetteFB@users.noreply.github.com>
2024-01-15 15:53:52 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,11 @@ int main(void)
 char currentLetter = 'a';
 while (currentLetter <= 'z')
 {
-while (currentLetter != 'e' || currentLetter != 'q')
-{
+
 putchar(currentLetter);
 currentLetter++;
 }
-}
+
 putchar('\n');
 return (0);
 }
