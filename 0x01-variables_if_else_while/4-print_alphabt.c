4cb36dcdfef119742f53a0341e09926412ec278b
YvetteFB <fionabaraza@gmail.com>
2024-01-15 04:43:58 -0800

    exclusion

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: returns 1
+*/
+int main(void)
+{
+char currentLetter = 'a';
+while (currentLetter <= 'z' && currentLetter != 'e' && currentLetter != 'q')
+{
+putchar(currentLetter);
+currentLetter++;
+}
+putchar('\n');
+return (0);
+}
