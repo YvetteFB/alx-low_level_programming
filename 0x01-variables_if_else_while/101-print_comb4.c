b177bf0452fad373560a22e8a8153904b0581ccb
Fiona Yvette Baraza <61788237+YvetteFB@users.noreply.github.com>
2024-01-16 10:22:09 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,15 +15,19 @@ for (i = 0; i <= 9; i++)
 {
 for (j = i + 1; j <= 9; j++)
 {
-if (i < j)
+for (k = j + 1; k <= 9; k++)
+{
+if (j < k)
 {
 putchar(i + '0');
 putchar(j + '0');
+putchar(k + '0');
 }
 if (!(i == 8 && j == 9))
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
