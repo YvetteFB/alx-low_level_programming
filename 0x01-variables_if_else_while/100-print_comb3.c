89524147e3f1e84864ffae94d80e5a715999ea11
Fiona Yvette Baraza <61788237+YvetteFB@users.noreply.github.com>
2024-01-16 10:14:27 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,9 +17,9 @@ for (j = i + 1; j <= 9; j++)
 if (i < j)
 {
 putchar(i + '0');
-putchar( j + '0');
+putchar(j + '0');
 }
-if(!(i ==8 && j ==9))
+if(!(i == 8 && j == 9))
 {
 putchar(',');
 putchar(' ');
@@ -28,5 +28,5 @@ putchar(' ');
 }
 putchar('\n');
 
-return 0;
+return (0);
 }
