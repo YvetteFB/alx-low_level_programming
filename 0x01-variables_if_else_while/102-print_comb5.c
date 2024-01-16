fcdf4f18896b1c1b8054b79f3c415695189b5441
Fiona Yvette Baraza <61788237+YvetteFB@users.noreply.github.com>
2024-01-16 10:57:28 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,25 +16,17 @@ for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-for (k = 0; k <= 9; k++)
+for (k = j; k <= 9; k++)
 {
-for (n = k + 1; n <= 9; n++)
-{
-if (i <= j)
+for (n = (k == i) ? j + 1 : 0; n <= 9; n++)
 {
 putchar(i + '0');
 putchar(j + '0');
-}
-
 putchar(' ');
-
-if (k < n)
-{
 putchar(k + '0');
 putchar(n + '0');
-}
 
-if (!(i == 8 && j == 9))
+if (!(i == 9 && j == 9 && k == 9 && n == 9))
 {
 putchar(',');
 putchar(' ');
