3f2f582749c6609fb9bfb2765019032122d2db9d
YvetteFB <fionabaraza@gmail.com>
2024-01-15 23:18:22 -0800

    print 3 digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+int i;
+int j;
+int k;
+ 
+for (i = 0; i <= 9; i++)
+{
+for (j = i + 1; j <= 9; j++)
+{
+if (i < j)
+{
+putchar(i + '0');
+putchar(j + '0');
+}
+if (!(i == 8 && j == 9))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
